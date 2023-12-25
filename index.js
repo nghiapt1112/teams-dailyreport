dd2f4d79059a2d228b7535803f49f2a0173a81a6
nghiapt1112 <nghiapt1112@gmail.com>
2023-12-25 15:14:48 +0700

    store to file

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+const fs = require('fs');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+
 var port = 9000;
 
-app.post('/post/data', function(req, res) {
+app.post('/post/data', function(request, response) {
     console.log('receiving data...');
-    console.log('body is ',req.body);
-    res.send(req.body);
+    console.log('body is ',request.body);
+
+
+
+    const path = '/home/ubuntu/project/jira/hooks/teams-dailyreport/hookdata';
+    // const path2 = '/Users/ptnghia1/data/sources/cmc/jira/jira-hook/';
+    fs.writeFile (path + "input.json", JSON.stringify(request.body), function(err) {
+            if (err) throw err;
+            console.log('complete');
+        }
+    );
+
+    //
+    // let body = '';
+    // const filePath = '/Users/ptnghia1/data/sources/cmc/jira/jira-hook/data.json';
+    // request.on('data', function(data) {
+    //     body += data;
+    // });
+    //
+    // request.on('end', function (){
+    //     fs.appendFile(filePath, body, function() {
+    //         respond.end();
+    //     });
+    // });
+
+    response.send(request.body);
 });
 
 // start the server
